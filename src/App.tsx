f8cfd39730b8dd52c174ef54aed7750f6cb060d4
Yegor Levandovskyi <yegor.levandovskyi@interactio.io>
2022-04-21 10:46:21 +0300

    Update example

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import {defineStore, useStore} from "./store";
+import styles from "./App.module.css";
 
-interface State {
-    counter: number;
-    message: string;
-}
-
-const initialState: State = {
+const initialState = {
     counter: 0,
     message: ""
 }
 
-const store = defineStore<State>(initialState);
+const store = defineStore(initialState);
 
 const resetStore = () => {
     store.setState(initialState);
@@ -20,11 +16,11 @@ export const WholeState = () => {
     const state = useStore(store);
 
     return (
-        <div style={{padding: "1rem", border: "1px solid blue"}}>
+        <section className={`${styles.section} ${styles.mb} ${styles.borderRed}`}>
             <div>Whole state component:</div>
 
             <div>{JSON.stringify(state)}</div>
-        </div>
+        </section>
     );
 };
 
@@ -38,13 +34,13 @@ export const Counter = () => {
     };
 
     return (
-        <div style={{padding: "1rem", border: "1px solid red"}}>
+        <section className={`${styles.section} ${styles.mb} ${styles.borderBlue}`}>
             <div>Counter component</div>
 
             <div>{counter}</div>
 
             <button onClick={inc}>Inc</button>
-        </div>
+        </section>
     );
 };
 
@@ -58,26 +54,30 @@ export const Message = () => {
     };
 
     return (
-        <div style={{padding: "1rem", border: "1px solid purple"}}>
+        <section className={`${styles.section} ${styles.mb} ${styles.borderOrange}`}>
             <div>Message component:</div>
 
             <div>
                 <input onChange={update} value={message} />
             </div>
-        </div>
+        </section>
     );
 };
 
+const ResetState = () => <section className={`${styles.section} ${styles.borderPink}`}>
+    <div>
+        Reset state component:
+    </div>
+    <button onClick={resetStore}>Reset</button>
+</section>
+
 export const App = () => {
     return (
-        <div>
+        <main>
             <Counter />
             <Message />
             <WholeState />
-
-            <br />
-
-            <button onClick={resetStore}>Reset</button>
-        </div>
+            <ResetState />
+        </main>
     );
 };
